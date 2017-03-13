55da476cd022ed51ca817a11ee657f82e304f0ab
Unknown <cmr648@nyu.edu>
2017-03-13 02:10:23 -0400

    Idols
    New enemy type, spinning idols

---
+++ Tech_Prototype_Test/Assets/Scripts/Enemy_Health/Idol_Health.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Idol_Health : MonoBehaviour {
+
+	public float Start_Health; // the health that an enemy will start with
+	float Half_Health;
+	public GameObject[] Treasure_Drops; // creating a public list of gameobjects for the enemys to drop when they die
+	int Treasure_Choose; // creating a treasure chooser variable to choose the treasure that will be droped by an enemy
+	public SpriteRenderer Enemy_Renderer; // Creating a variable to get the renderer of the enemy game object
+	public SpriteRenderer Mouth_Renderer; // Creating a variable to get the renderer of the mouth of the enemy game object
+	public SpriteRenderer Top_Teeth_Renderer;// Creating a variable for our top teeth renderer
+	public SpriteRenderer Bottom_Teeth_Renderer; // Creating a variable for our bottom teeth renderer
+	public SpriteRenderer Eye_Renderer_Right; // Creatinga  renderer for our right eye
+	public SpriteRenderer Eye_Renderer_Left; // Creating a rendere for our left eye
+	public ParticleSystem Enemy_Death_Particle; //Creating a public enemy death particle system to instantiate upon death
+
+
+	// Use this for initialization
+	void Start () {
+	Eye_Renderer_Left.color = Color.black; // setting our left eye to be black
+	Eye_Renderer_Right.color = Color.black; // setting our right eye to be black
+
+	Half_Health = Start_Health/2; // setting half health to be half the player health
+
+	Dungeon_Clear_Checker.Enemy_Amount = Dungeon_Clear_Checker.Enemy_Amount +1; // adding one to the enemy amount for every enemy on screen
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate () // an update function that is called every frame
+	{
+
+		Treasure_Choose = Random.Range(0,Treasure_Drops.Length); // setting our treasure choose variable to be a different treasure every time an enemy dies
+
+		if (Start_Health <= Half_Health) { // checking to see if the enemy health is less than or equal to 2
+			Enemy_Renderer.color = Color.blue; // assinging the red color to the enemy
+			Mouth_Renderer.color = Color.black; // assinging the black color to the enemy mouth
+			Top_Teeth_Renderer.color = Color.red; // assinging a red color to the top teeth
+			Bottom_Teeth_Renderer.color = Color.red; // assinging a red color to the bottom teeth
+			Eye_Renderer_Right.color = Color.red; // assinging a red color to the right eye
+			Eye_Renderer_Left.color = Color.red; // assigining a red color to the left yee
+		}
+
+		if (Start_Health <= 0) { // checking to see if the enemy has no life left
+		Destroy(gameObject); // destroy the enemy game object
+		Instantiate(Enemy_Death_Particle,transform.position,Quaternion.identity); // instantiating the enemy death particle system upon enemy death
+		Dungeon_Clear_Checker.Enemy_Amount = Dungeon_Clear_Checker.Enemy_Amount -1; // subtracting the enemy from the enemy amount variable
+		Instantiate(Treasure_Drops[Treasure_Choose],transform.position,Quaternion.identity); // dropping the treasure in the place of the enemy when they die
+		}
+
+	}
+
+	void OnCollisionEnter2D (Collision2D col)	{ // a void that checks to see if the enemy collided with soething
+		if (col.gameObject.tag == "Player_Bullet") { // checking to see if the collision was with a player bullet
+			Start_Health -= 1; // make the enemy lose health
+			Destroy(col.gameObject);// destroyt the player bullet
+			Player_Shooting.Bullet_Limit = Player_Shooting.Bullet_Limit -1; // subtracting 1 from the player bullet limit
+		}
+
+
+	}
+}
