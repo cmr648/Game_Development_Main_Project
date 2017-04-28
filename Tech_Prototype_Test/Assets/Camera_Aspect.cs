190dca5dd85725cc994325b21210bdf2e5719a2e
Unknown <cmr648@nyu.edu>
2017-04-28 05:37:52 -0400

    Game now works in all aspect ratios

---
+++ Tech_Prototype_Test/Assets/Camera_Aspect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Aspect : MonoBehaviour {
+	float Original_Width = 1600;
+	float Original_Height = 900;
+
+	// Use this for initialization
+	void Start () {
+
+	gameObject.GetComponent<Camera>().aspect = (Original_Width/Original_Height) * (Screen.width/Screen.height);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
